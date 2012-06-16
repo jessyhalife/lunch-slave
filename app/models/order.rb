5fcc8a8b979cdb81964a1e3977bcaaf3251e44a3
Marcia Paez <marcia.paez@gmail.com>
2012-06-16 14:51:37 -0300

    Accessible attributes for Order
    Accessible attributes for Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :user_id, :date, :type, :detail, :menu_id, :place_id
   belongs_to :user
   belongs_to :menu
   belongs_to :place
