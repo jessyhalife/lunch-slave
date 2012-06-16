8421f7320723dd8feae51a12951c8b3cc913fa9a
Ami <amireynoso@gmail.com>
2012-06-16 14:51:24 -0300

    attr_accessible menu / taste

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,3 @@
 class Menu < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :description
 end
--- app/models/taste.rb
+++ app/models/taste.rb
@@ -1,4 +1,4 @@
 class Taste < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :user_id, :like, :description
   belongs_to :user
 end
