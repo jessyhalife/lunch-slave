360a478fa07a0bc08e6f45f9cb11c77b09f189e9
jessyhalife <cazadora@gmail.com>
2012-06-16 14:17:41 -0300

    Updated migrations (slaves and users)

--- db/migrate/20120616160653_create_users.rb
+++ db/migrate/20120616160653_create_users.rb
@@ -1,8 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :password_digest
+      t.string :email
+      t.string :diet
       t.timestamps
     end
   end
 end
+
--- db/migrate/20120616160722_create_slaves.rb
+++ db/migrate/20120616160722_create_slaves.rb
@@ -1,7 +1,8 @@
 class CreateSlaves < ActiveRecord::Migration
   def change
     create_table :slaves do |t|
-
+      t.date :date
+      t.references :user
       t.timestamps
     end
   end
