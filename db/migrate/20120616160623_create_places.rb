b19b54e15c6867dbb240d40e05da9886af7c9d3b
valerialaura <valerialaura@gmail.com>
2012-06-16 14:14:26 -0300

    created table definition

--- db/migrate/20120616160623_create_places.rb
+++ db/migrate/20120616160623_create_places.rb
@@ -1,7 +1,10 @@
 class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
-
+   	  t.string :name
+      t.string :address
+      t.string :cuisine
+      t.string :website
       t.timestamps
     end
   end
