71b8804d448849a287f8800dc84975b18b929c5d
Ami <amireynoso@gmail.com>
2012-06-16 14:15:33 -0300

    Migration: Tastes

--- db/migrate/20120616160726_create_tastes.rb
+++ db/migrate/20120616160726_create_tastes.rb
@@ -1,7 +1,9 @@
 class CreateTastes < ActiveRecord::Migration
   def change
     create_table :tastes do |t|
-
+      t.references :user
+      t.boolean :like
+      t.text :description
       t.timestamps
     end
   end
