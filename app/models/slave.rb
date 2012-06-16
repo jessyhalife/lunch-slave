9afde8347ecf8d7800b182e957bff90bcd4d5ba1
jessyhalife <cazadora@gmail.com>
2012-06-16 14:38:05 -0300

    Updated slave and user model

--- app/models/slave.rb
+++ app/models/slave.rb
@@ -1,3 +1,4 @@
 class Slave < ActiveRecord::Base
   # attr_accessible :title, :body
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
+  has_many :orders
+  has_many :tastes
 end
