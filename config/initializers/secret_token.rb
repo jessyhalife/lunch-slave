e0419193ffd4872d3ae1dce587ed7e5e40e7619c
jessyhalife <cazadora@gmail.com>
2012-06-16 12:43:30 -0300

    added rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchSlave::Application.config.secret_token = '5d73346a457041a1f0cb6109db0154b4be1c51988e8dcb01e30fd0afeef234f196eadf6e3d49dee01d537eb21c13fa8e66e287e9cee9aa1ad9bc788aad554a96'
